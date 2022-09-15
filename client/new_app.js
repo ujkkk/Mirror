b3b32f8b4c6e49f59dfb3090b593a701d294a409
ujkkk <jean207@naver.com>
2022-09-15 18:19:01 +0900

    유진

--- client/new_app.js
+++ client/new_app.js
@@ -1,5 +1,6 @@
 // 통화 모듈
 const callAccess = require('./call_module/call')
+const moment = require('moment');
 // 날씨 모듈 불러오기
 require('./weather_module/new_weather')
 const fs = require('fs');
@@ -14,6 +15,7 @@ mirrorDB.setUser(receivedData)
         require("./message_module/message_icon.js")
         require('./memo_module/memo')
         require('./new_callbook')
+   
 
     const message = require('./message_module/message')
     message.initMessages()
@@ -41,13 +43,13 @@ axios.get(`http://113.198.84.128:80/check/${mirrorDB.getId()}`)
         }
 
         for (let i = 0; i < response.data.contents.length; i++) {
-            console.log(datas[i]);
+            var send_time = moment(datas[i].send_time).format('YYYY-MM-DD HH:mm:ss');
             let data = {
                 sender: datas[i].sender,
                 receiver: mirrorDB.getId(),
                 content: datas[i].content,
                 type: '',
-                send_time: datas[i].send_time
+                send_time: send_time
             };
             console.log(data);
             //DB에 삽입
